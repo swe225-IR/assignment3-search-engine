a333d30dc8392805c2b22a69c5e63248739089b3
lori <11811407@mail.sustech.edu.cn>
2023-02-24 15:29:21 -0800

    refactor

--- utils/build_index.py
+++ utils/build_index.py
@@ -5,22 +5,22 @@ import pickle
 
 class Database:
     def __init__(self, word_save_path, tfidf_path):
-        '''
+        """
         data structure:
             {sub-folder(): [link_file_name1, ...]}
             eg. {xxx_xxx_xxx_edu: [xxxxxxxx.pickle, xxxxxx.pickle, ...]}
-        '''
+        """
         self.word_save_path = word_save_path  # '../data/index/words_index/'
         self.tfidf_path = tfidf_path  # '../data/index/tfidf/'
         self.temp_word_save_path = word_save_path  # # '../data/index/words_index/a/b/c/'
         self.word_info = dict()
 
     def save(self, pkl_file, tfidf_sub_folder, word_info):
-        '''
+        """
         :param pkl_file: xxxxx.pickle
         :param tfidf_sub_folder: xxx_xxx_xxx_edu
         :param word_info: {word: [tf, idf]}
-        '''
+        """
         for word in word_info:
             if not word.isalnum():
                 continue
@@ -28,11 +28,11 @@ class Database:
             self.temp_word_save_path = self.word_save_path
             self.valid_folder(word)
             if not os.path.exists(self.temp_word_save_path + '/' + word + '.pickle'):
-                with open(self.temp_word_save_path + '/' + word + '.pickle', 'wb') as f:
+                with open(self.temp_word_save_path + '/' + word + '.pickle', 'wb') as fi:
                     # new_info_dic = dict()
                     # new_info_dic[tfidf_sub_folder] = [pkl_file]
                     # pickle.dump(new_info_dic, f)
-                    pickle.dump([tfidf_sub_folder, pkl_file], f)
+                    pickle.dump([tfidf_sub_folder, pkl_file], fi)
                     continue
 
             # with open(self.temp_word_save_path + '/' + word + '.pickle', 'rb') as f:
@@ -45,13 +45,13 @@ class Database:
 
             # with open(self.temp_word_save_path + '/' + word + '.pickle', 'wb') as f:
             #     pickle.dump(saved_info_dic, f)
-            with open(self.temp_word_save_path + '/' + word + '.pickle', 'ab') as f:
-                pickle.dump([tfidf_sub_folder, pkl_file], f)
+            with open(self.temp_word_save_path + '/' + word + '.pickle', 'ab') as fi:
+                pickle.dump([tfidf_sub_folder, pkl_file], fi)
 
     def valid_folder(self, word):
-        '''
+        """
         Creat tree folder for word
-        '''
+        """
         if not os.path.exists(self.temp_word_save_path):
             os.mkdir(self.temp_word_save_path)
 
@@ -69,9 +69,9 @@ class Database:
 
 
 def read_pkl(path):
-    '''
+    """
     :return: word information. eg. {word: [tf, idf]}
-    '''
+    """
     with open(path, 'rb') as f:
         page_info = pickle.load(f)
         for k in page_info:
