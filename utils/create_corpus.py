082fa0f7d3bc3d607769b114611095ba20c253dd
Fred <yangl73@uci.edu>
2023-02-23 23:43:03 -0800

    Modify tfidf data structure

--- utils/create_corpus.py
+++ utils/create_corpus.py
@@ -1,26 +1,32 @@
 import json
 import os
+
+
 from collections import Counter
 
 
 if __name__ == '__main__':
-    root_dir = 'D:/UCI/1-Q2/CS221/Assignment-3-web-crawler/assignment3-search-engine/METADATA/'
-    corpus_path = 'D:/UCI/1-Q2/CS221/Assignment-3-web-crawler/assignment3-search-engine/corpus.json'
-    rel_files = []
-    corpus = Counter()
+    root_dir = '../METADATA/'
+    corpus_path = '../data/index/corpus.json'
+    global_info_path = '../data/index/global_info.json'
 
+    page_number = 0  # page number in total
+    corpus = Counter()
     for dir_, _, files in os.walk(root_dir):
         for file_name in files:
             rel_dir = os.path.relpath(dir_, root_dir)
             rel_file = os.path.join(rel_dir, file_name)
-            rel_files.append(rel_file)
+            page_number += 1
             with open(root_dir + rel_file, 'r') as f:
                 page = json.load(f)
             word_per_page = page[[*page][0]]['word_positions']
-            for word in word_per_page:
+            for word in word_per_page:  # Calculate the number of pages that contains word.
                 corpus[word] += 1
-
-    corpus['total page number'] = len(rel_files)
 
     with open(corpus_path, "w") as outfile:
         json.dump(corpus, outfile, indent=4)
+
+    with open(global_info_path, "w") as outfile:
+        json.dump(page_number, outfile, indent=4)
+
+    print('Page_number: ' + str(page_number) + '\n' + 'Done')
--- utils/ranking.py
+++ utils/ranking.py
@@ -6,17 +6,22 @@ import numpy as np
 import os
 from collections import Counter
 
+
 class TFIDF:
-    def __init__(self, word_frequencies: Dict[str, int], corpus: Dict[str, int], weights: list):
-        self.word_frequencies = word_frequencies
-        self.word_counts = 0
+    def __init__(self,
+                 corpus: Dict[str, int],
+                 page_number: int,
+                 weights=[1, 4, 7, 8, 9, 10, 15]):
+
         self.corpus = corpus
-        self.corpus_count = self.corpus['total page number']
+        self.page_number = page_number
         self.weights = np.array(weights)
+        self.word_counts = 0
         self.word_tf = {}
         self.word_idf = {}
-        self.word_if_idf = {}
-    
+        self.word_tf_idf = {}
+        self.tf_idf_result = {}  # {word: [tf, idf]}
+
     def calculate_weighted_frequency(self, k, v):
         v = np.array(v)
         v[0] -= np.sum(v[1:])
@@ -24,47 +29,82 @@ class TFIDF:
         self.word_tf[k] = weighted_freq
         self.word_counts += weighted_freq
 
-    def calculate_tf(self):
-        for k, v in self.word_frequencies.items():
+    def calculate_tf(self, word_frequencies):
+        for k, v in word_frequencies.items():
             self.calculate_weighted_frequency(k, v)
+
         for k in self.word_tf.keys():
             self.word_tf[k] /= self.word_counts
 
-    def calculate_idf(self):
-        for k in self.word_frequencies.keys():
+            self.tf_idf_result[k] = [0, 0]
+            self.tf_idf_result[k][0] = self.word_tf[k]
+
+    def calculate_idf(self, word_frequencies):
+        for k in word_frequencies.keys():
             num_of_corpus_contain_k = self.corpus[k]
-            self.word_idf[k] = math.log(self.corpus_count / (num_of_corpus_contain_k + 1))
+            self.word_idf[k] = math.log(self.page_number / (num_of_corpus_contain_k + 1))
+
+            self.tf_idf_result[k][1] = self.word_idf[k]
 
     def calculate_if_idf(self):
-        self.calculate_tf()
-        self.calculate_idf()
         for k, v in self.word_tf.items():
-            self.word_if_idf[k] = v * self.word_idf[k]
+            self.word_tf_idf[k] = v * self.word_idf[k]
 
-        self.word_features = Counter(self.word_if_idf)
+    def calculate(self, word_frequencies):
+        self.calculate_tf(word_frequencies)
+        self.calculate_idf(word_frequencies)
+        # self.calculate_if_idf()
 
-    def output_page_feature_words(self, fpath):
-        if not os.path.exists(fpath.split('\\')[0]):
-            os.mkdir(fpath.split('\\')[0]) 
-        with open(fpath, 'w') as f:
-            json.dump(self.word_features, f, indent=4)
+    def save(self, folder_path, sub_folder, file_name, url):
+        if not os.path.exists(folder_path + sub_folder):
+            os.mkdir(folder_path + sub_folder)
 
-# %%
+        save_dic = {url: self.tf_idf_result}
+        with open(folder_path + file_name, 'w') as f:
+            json.dump(save_dic, f, indent=4)
+
+    def reset(self):
+        self.word_counts = 0
+        self.word_tf = {}
+        self.word_idf = {}
+        self.word_tf_idf = {}
+        self.tf_idf_result = {}
+
+
+def read_json(path):
+    with open(path, 'r') as f:
+        page = json.load(f)
+        for key in page:
+            url = key
+            break
+
+        return [url, page[[*page][0]]['word_frequency_weights']]
+
+
 if __name__ == '__main__':
-    with open('../corpus.json', 'r') as f:
+    print('Loading corpus...')
+    with open('../data/index/corpus.json', 'r') as f:
         corpus = json.load(f)
 
-    root_dir = 'D:/UCI/1-Q2/CS221/Assignment-3-web-crawler/assignment3-search-engine/METADATA/'
-    output_dir = 'D:/UCI/1-Q2/CS221/Assignment-3-web-crawler/assignment3-search-engine/tfidf/'
-    weights = [1, 4, 7, 8, 9, 10, 15]
+    with open('../data/index/global_info.json', 'r') as f:
+        page_number = json.load(f)
 
+    root_dir = '../METADATA/'
+    output_dir = '../data/index/tfidf/'
+
+    print('Start calculating...')
+    page_progress = 0
+    tfidf_calculator = TFIDF(corpus, page_number)
     for dir_, _, files in os.walk(root_dir):
         for file_name in files:
             rel_dir = os.path.relpath(dir_, root_dir)
             rel_file = os.path.join(rel_dir, file_name)
-            with open(root_dir + rel_file, 'r') as f:
-                page = json.load(f)
-            TFIDF_calc = TFIDF(page[[*page][0]]['word_frequency_weights'], corpus, weights)
-            TFIDF_calc.calculate_if_idf()
-            TFIDF_calc.output_page_feature_words(output_dir + rel_file)
-#%%
+            [url, word_frequencies] = read_json(root_dir + rel_file)
+
+            tfidf_calculator.calculate(word_frequencies)
+            tfidf_calculator.save(output_dir, rel_dir, rel_file, url)
+            tfidf_calculator.reset()
+
+            page_progress += 1
+            if page_progress % 500 == 0:
+                print('Progress: ' + str(int((page_progress / page_number) * 100)) + '%')
