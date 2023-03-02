311cd0fd228e475a18f52b4e051615e108f065dc
Fred <yangl73@uci.edu>
2023-03-01 19:01:33 -0800

    Update link index method because of the change of the structure of link indexer

--- utils/indexer.py
+++ utils/indexer.py
@@ -15,31 +15,70 @@ class Indexer:
 
     def word_indexer(self, word: str):
         '''
-        :return: relative path. eg. ../data/index/words_index/a/b/c/abc.pickle
+        :return: relative path. eg. ../data/index/words_index/a/b/c/
         '''
+
         word_path = self.words_index_path
         for index in range(0, 3):
             if index > len(word):
                 break
 
             word_path = os.path.join(word_path, word[index: index + 1])
-
         return word_path
 
-    def link_indexer(self, paths: list, word: str):
+    def link_indexer(self, locations_info: list, word: str):
         '''
+        :param locations_info: All locations info we need to find.
+                          eg. [[domain1, folder_name1], [domain2, folder_name2], ...]
         :return: The tf and idf socre of the word in every page it appears.
                  eg. {url1: [tf, idf], url2: [tf, idf]}
         '''
         link_info_s = {}
-        for path in paths:
-            with open(path, 'rb') as f:
-                link_info = pickle.load(f)
-                for k in link_info:
-                    url = k
-                    break
-                link_info_s[url] = link_info[url].get(word, None)
+        urls = []
+        tf_idf_scores = []
+        tf_idf_paths = self.path_transformer(locations_info, word)
+        url_paths = self.get_urls(locations_info)
+
+        for url_path in url_paths:
+            with open(url_path, 'rb') as f:
+                urls.append(pickle.load(f))
+
+        for tf_idf_path in tf_idf_paths:
+            with open(tf_idf_path, 'rb') as f:
+                tf_idf_scores.append(pickle.load(f))
+
+        for index in range(len(urls)):
+            link_info_s[urls[index]]  = tf_idf_scores[index]  # {url: [tf, idf]}
+
         return link_info_s
+
+    def path_transformer(self, locations_info: list, word: str):
+        '''
+        :return: A list contains all path we need to find in link indexer.
+                 eg. [tfidf/domain/url_folder_name/a/b/c/word.pickle, ....]
+        '''
+        word_path_in_link_index = word[0]
+        paths = []
+        for index in (range(1, 3) if len(word) >= 3 else range(1, len(word))):
+            word_path_in_link_index = os.path.join(word_path_in_link_index, word[index])
+
+        word_path_in_link_index = os.path.join(word_path_in_link_index, word + '.pickle')  # .../.../.../a/b/c/word.pickle
+
+        for index in range(len(locations_info)):
+            paths.append(os.path.join(self.tfidf_path, locations_info[index][0], locations_info[index][1],
+                                            word_path_in_link_index))  # tfidf/domain/url_folder_name/a/b/c/word.pickle
+
+        return paths
+
+    def get_urls(self, locations_info:list):
+        '''
+        :return: Return the paths of the pickle file which contains url information.
+        '''
+        url_paths = []
+        for info in locations_info:
+            url_paths.append(os.path.join(self.tfidf_path, info[0], info[1], 'URL_info.pickle'))
+
+        return url_paths
 
     def standardize_(self, query: str, stemmer=SnowballStemmer(language="english")):
         word_list = []
@@ -51,28 +90,25 @@ class Indexer:
             word_list.append(word)
         return word_list
 
-    def get_urls(self, query: str):
+    def get_tf_idf(self, query: str):
+        link_index_locations_info = []
         answer_pages_info = {}
         words = self.standardize_(query)
         for word in words:
-            path = self.word_indexer(word)
-
-            path = os.path.join(path, word + '.pickle')
-            paths = list()
-
-            if not os.path.exists(path):
+            word_path = self.word_indexer(word)
+            word_path = os.path.join(word_path, word + '.pickle')
+            if not os.path.exists(word_path):
                 continue
 
-            with open(path, 'rb') as f:
+            with open(word_path, 'rb') as f:  # Open word index .pkl file to get link index location
                 while True:
                     try:
-                        link_id = pickle.load(f)
-                        link_sub_path = self.tfidf_path + link_id[0] + '/' + link_id[1]
-                        paths.append(link_sub_path + '.pickle')
+                        location_info = pickle.load(f)  # [doamin, folder_name]
+                        link_index_locations_info.append(location_info)
                     except:
                         break
 
-            link_info = self.link_indexer(paths, word)
+            link_info = self.link_indexer(link_index_locations_info, word)
             answer_pages_info[word] = link_info
 
         # pprint.pprint(answer_pages_info)
